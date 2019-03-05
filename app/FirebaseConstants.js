52aae98b97a25c9c8d3d6eef7f7f0e72285e1c99
Hamza Ali <hamxa1331@gmail.com>
2019-03-06 01:11:15 +0500

    Initial Commit
    Basic working app implemented for remote push notifications.

---
+++ app/FirebaseConstants.js
@@ -0,0 +1,6 @@
+
+const FirebaseConstants = {
+	KEY: "AAAA53B-adk:APA91bFu90TCn11KnnO_PPW4T98-Rnmekv2s-XXHEtSVOKhEw531o_ld2RX_KSPWUMpHAXGsgNt6mQvrJuvJR_ITbfAOFFG_rtz2JcSViRcryGqurOajy2g4_sYuuKQNGNa2Hpnfvsi5"
+}
+
+export default FirebaseConstants;
